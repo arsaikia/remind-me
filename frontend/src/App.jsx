56594ef76a2fed2ca937a65d89dcc49bd9e688f2
Arunabh Saikia <arunabh.matrix@gmail.com>
2023-06-20 20:10:50 -0700

    in progress

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,30 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { getQuestions } from './actions/actions';
-import { useState, useEffect } from 'react';
-import Tab from './components/Tab'
-import { useCookies } from 'react-cookie';
+import { useSelector, useDispatch } from "react-redux";
+import { useState, useEffect } from "react";
+import { useCookies } from "react-cookie";
 
-import { Route, Navigate, BrowserRouter as Router, Routes } from 'react-router-dom';
-import { Container } from './styles';
-import Navbar from './components/Navbar';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import Playground from './pages/Playground';
-import AllRoutes from './AllRoutes';
-import Error from './pages/Error';
+import {
+  Route, Navigate, BrowserRouter as Router, Routes,
+} from "react-router-dom";
+import Tab from "./components/Tab";
+import { getQuestions } from "./actions/actions";
+import { Container } from "./styles";
+import Navbar from "./components/Navbar";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Playground from "./pages/Playground";
+// import AllRoutes from "./AllRoutes";
+import Error from "./pages/Error";
 
-
-const App = () => {
-
-  const [cookies, setCookie, removeCookie] = useCookies(['userId', 'openTab', 'name']);
+function App() {
+  const [cookies, setCookie, removeCookie] = useCookies(["userId", "openTab", "name"]);
 
   // Get states using useSelector ( state->reducerName )
-  const allQuestions = useSelector(state => state.questions.allQuestions);
-  const solvedQuestions = useSelector(state => state.questions.solvedQuestions);
-  const todoQuestions = useSelector(state => state.questions.todoQuestions);
-  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
-  const userIdInAuthStore = useSelector(state => state.auth.userId);
-  const userNameIdInAuthStore = useSelector(state => state.auth.firstName);
+  const allQuestions = useSelector((state) => state.questions.allQuestions);
+  // const solvedQuestions = useSelector((state) => state.questions.solvedQuestions);
+  const todoQuestions = useSelector((state) => state.questions.todoQuestions);
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const userIdInAuthStore = useSelector((state) => state.auth.userId);
+  const userNameIdInAuthStore = useSelector((state) => state.auth.firstName);
 
   const userIdInCookie = cookies.userId;
   const userId = userIdInCookie || userIdInAuthStore;
@@ -38,81 +38,77 @@ const App = () => {
   // Load all question when the app first loads/ user signs in
   useEffect(() => {
     fetchAllQuestions(userId);
-  }, [userId])
-  
+  }, [userId]);
+
   // Set cookie when user logs in
   useEffect(() => {
     if (!isAuthenticated) {
-      return
+      return;
     }
-    setCookie('userId', userId, { path: '/' });
-    setCookie('name', userNameIdInAuthStore, { path: '/' });
-  }, [isAuthenticated])
-  
-  
+    setCookie("userId", userId, { path: "/" });
+    setCookie("name", userNameIdInAuthStore, { path: "/" });
+  }, [isAuthenticated]);
 
-  /**************************************************************
+  /** ************************************************************
    * Handler Functions
-  **************************************************************/
-  
-const Todo = () => {
+  ************************************************************* */
 
-  return (
-    <>
-    <div>
-      <h2>Today's</h2>
-      <Tab data={todoQuestions} isOpen isRecap userId={userId} />
-    </div>
-  </>)
-}
-
-const All = () => {
-
-  return (
-    <div>
-      <h2>Questions</h2>
+  function Todo() {
+    return (
       <div>
-        {
-          allQuestions.groups.map( group => <Tab
-            key={group}
-            data={allQuestions.questions[group]}
-            group={group}
-            userId={userId}
-            isOpen={tabCookie === group}
-            tabCookie={tabCookie}
-          />
-        )}
+        <h2>Today's</h2>
+        <Tab data={todoQuestions} isOpen isRecap userId={userId} />
       </div>
-    </div>
-  )
-}
-  
-  /**************************************************************
-   * Returns JSX from here
-  **************************************************************/
+    );
+  }
 
+  function All() {
+    return (
+      <div>
+        <h2>Questions</h2>
+        <div>
+          {
+            allQuestions.groups.map((group) => (
+              <Tab
+                key={group}
+                data={allQuestions.questions[group]}
+                group={group}
+                userId={userId}
+                isOpen={tabCookie === group}
+                tabCookie={tabCookie}
+              />
+            ))
+          }
+        </div>
+      </div>
+    );
+  }
+
+  /** ************************************************************
+   * Returns JSX from here
+  ************************************************************* */
 
   return (
     <Router>
       <Navbar />
       {/* Reserved space taken by the absolute Navbar */}
       <Container
-        width={"100%"}
-        height={"8vh"}
+        width="100%"
+        height="8vh"
       />
 
-      <Container width={'90%'} padding={'0 5%'}>
+      <Container width="90%" padding="0 5%">
         <Routes>
-          <Route path="/" element={ <Todo/>} />
-          <Route path="/signup" element={ <Signup/>} />
-          <Route path="/login" element={ <Login/>} />
+          <Route path="/" element={<Todo />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/all" element={<All />} />
-          <Route path="/playground" element={ <Playground />} />
+          <Route path="/playground" element={<Playground />} />
           <Route path="/404" element={<Error />} />
           <Route path="*" element={<Navigate replace to="/404" />} />
         </Routes>
       </Container>
-      
+
     </Router>
   );
 }
