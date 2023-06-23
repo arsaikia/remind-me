46d1ac3cdde98a506b8bea6736bae4df298a150f
Arunabh Saikia <arunabh.matrix@gmail.com>
2023-06-22 22:04:23 -0700

    update data model and queries

--- backend/seeder.js
+++ backend/seeder.js
@@ -1,14 +1,15 @@
 import fs from 'fs';
 import colors from 'colors';
 import dotenv from 'dotenv';
+import { v4 as UUID_V4 } from 'uuid';
 
 import mongoose from 'mongoose';
 
 const MongoDB = async () => {
-    const MONGO_URI = '';
+    const MONGO_URI = 'mongodb+srv://admin:BuHPstzyufv8vaHk@prep-algo-dev.6ozmn1a.mongodb.net/?retryWrites=true&w=majority';
     const MONGO_URI_PROD = '';
     try {
-        const conn = await mongoose.connect(MONGO_URI_PROD, {
+        const conn = await mongoose.connect(MONGO_URI, {
             useUnifiedTopology: true,
             useNewUrlParser: true,
             useCreateIndex: true,
@@ -40,8 +41,13 @@ const questions = JSON.parse(fs.readFileSync(`./_data/questions.json`, 'utf-8'))
 const importData = async () => {
     try {
         for (let idx = 0; idx < questions.length; idx++) {
-            const { name, link, group, difficulty } = questions[idx];
-            await Question.create({ name, link, group, difficulty });
+            const {
+                name,
+                link,
+                group,
+                difficulty
+            } = questions[idx];
+            await Question.create({ _id: UUID_V4(), name, link, group, difficulty });
         }
 
         console.log('Data Imported...'.green.inverse);
